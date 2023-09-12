828ce4d725837f6c81199c8b2fb724a43297a19f
jordiPT9 <jordipages9a@gmail.com>
2023-09-12 19:44:05 +0200

    Recalculate typings script done

--- csv_utils.py
+++ csv_utils.py
@@ -1,4 +1,5 @@
 import csv
+import type_utils
 
 def remove_columns_from_csv(input_file, output_file, columns_to_remove):
     with open(input_file, 'r', newline='', encoding='utf-8') as infile, \
@@ -14,16 +15,113 @@ def remove_columns_from_csv(input_file, output_file, columns_to_remove):
             writer.writerow(new_row)
 
     print("CSV file has been modified. Removed columns:", columns_to_remove)
+    
+def recalculate_effectiveness(csv_file, pokemon_list): 
+    with open(csv_file, 'r', encoding='utf-8') as file:
+        csv_reader = csv.DictReader(file)
+        rows = list(csv_reader)
+
+    for row in rows:
+        if row['name'] in pokemon_list:
+            print("---")
+            print(f"{row['name']}: {row['type_1']} - {row['type_2']}")
+            print("")
+            print('')
+            row['against_normal'] = type_utils.get_multiplier("Normal", [row['type_1'], row['type_2']])
+            print(f"against_normal: {row['against_normal']}")
+            print('')
+            row['against_fire'] = type_utils.get_multiplier("Fire", [row['type_1'], row['type_2']])
+            print(f"against_fire: {row['against_fire']}")
+            print('')
+            row['against_water'] = type_utils.get_multiplier("Water", [row['type_1'], row['type_2']])
+            print(f"against_water: {row['against_water']}")
+            print('')
+            row['against_electric'] = type_utils.get_multiplier("Electric", [row['type_1'], row['type_2']])
+            print(f"against_electric: {row['against_electric']}")
+            print('')
+            row['against_grass'] = type_utils.get_multiplier("Grass", [row['type_1'], row['type_2']])
+            print(f"against_grass: {row['against_grass']}")
+            print('')
+            row['against_ice'] = type_utils.get_multiplier("Ice", [row['type_1'], row['type_2']])
+            print(f"against_ice: {row['against_ice']}")
+            print('')
+            row['against_fight'] = type_utils.get_multiplier("Fighting", [row['type_1'], row['type_2']])
+            print(f"against_fight: {row['against_fight']}")
+            print('')
+            row['against_poison'] = type_utils.get_multiplier("Poison", [row['type_1'], row['type_2']])
+            print(f"against_poison: {row['against_poison']}")
+            print('')
+            row['against_ground'] = type_utils.get_multiplier("Ground", [row['type_1'], row['type_2']])
+            print(f"against_ground: {row['against_ground']}")
+            print('')
+            row['against_flying'] = type_utils.get_multiplier("Flying", [row['type_1'], row['type_2']])
+            print(f"against_flying: {row['against_flying']}")
+            print('')
+            row['against_psychic'] = type_utils.get_multiplier("Psychic", [row['type_1'], row['type_2']])
+            print(f"against_psychic: {row['against_psychic']}")
+            print('')
+            row['against_bug'] = type_utils.get_multiplier("Bug", [row['type_1'], row['type_2']])
+            print(f"against_bug: {row['against_bug']}")
+            print('')
+            row['against_rock'] = type_utils.get_multiplier("Rock", [row['type_1'], row['type_2']])
+            print(f"against_rock: {row['against_rock']}")
+            print('')
+            row['against_ghost'] = type_utils.get_multiplier("Ghost", [row['type_1'], row['type_2']])
+            print(f"against_ghost: {row['against_ghost']}")
+            print('')
+            row['against_dragon'] = type_utils.get_multiplier("Dragon", [row['type_1'], row['type_2']])
+            print(f"against_dragon: {row['against_dragon']}")
+            print('')
+            row['against_dark'] = type_utils.get_multiplier("Dark", [row['type_1'], row['type_2']])
+            print(f"against_dark: {row['against_dark']}")
+            print('')
+            row['against_steel'] = type_utils.get_multiplier("Steel", [row['type_1'], row['type_2']])
+            print(f"against_steel: {row['against_steel']}")
+            print('')
+            row['against_fairy'] = type_utils.get_multiplier("Fairy", [row['type_1'], row['type_2']])
+            print(f"against_fairy: {row['against_fairy']}")
+    
+    with open(csv_file, 'w', newline='', encoding='utf-8') as file:
+        fieldnames = csv_reader.fieldnames
+        csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
+        
+        # Write the header
+        csv_writer.writeheader()
+        
+        # Write the updated rows
+        csv_writer.writerows(rows)
+
 
 # Example usage:
-input_csv = 'input.csv'
-output_csv = 'pokemon.csv'
+# input_csv = 'input.csv'
+# output_csv = 'pokemon.csv'
 
-columns_to_remove = [
-    '#', 'german_name', 'japanese_name', 'status', 'species', 'type_number',
-    'height_m', 'weight_kg', 'abilities_number', 'base_friendship',
-    'base_experience', 'growth_rate', 'egg_type_number', 'egg_type_1',
-    'egg_type_2', 'percentage_male', 'egg_cycles'
+# columns_to_remove = [
+#     '#', 'german_name', 'japanese_name', 'status', 'species', 'type_number',
+#     'height_m', 'weight_kg', 'abilities_number', 'base_friendship',
+#     'base_experience', 'growth_rate', 'egg_type_number', 'egg_type_1',
+#     'egg_type_2', 'percentage_male', 'egg_cycles'
+# ]
+
+# remove_columns_from_csv(input_csv, output_csv, columns_to_remove)
+pokemon_list = [
+    'Mawile',
+    'Azurill',
+    'Gardevoir',
+    'Kirlia',
+    'Ralts',
+    'Granbull',
+    'Snubbull',
+    'Azumarill',
+    'Marill ',
+    'Togetic',
+    'Togepi',
+    'Igglybuff',
+    'Jigglypuff',
+    'Wiglytuff',
+    'Mr. Mime',
+    'Clefairy',
+    'Clefable',
+    'Cleffa'
 ]
-
-remove_columns_from_csv(input_csv, output_csv, columns_to_remove)
+recalculate_effectiveness('pokemon.csv', pokemon_list)
--- type_utils.py
+++ type_utils.py
@@ -38,23 +38,26 @@ TYPE_CHART = [[1, 1, 1, 1, 1, 0.5, 1, 0, 0.5, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 [1, 0.5, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 0.5, 0.5],
 [1, 2, 1, 0.5, 1, 1, 1, 1, 0.5, 0.5, 1, 1, 1, 1, 1, 2, 2, 1]]
 
-def get_multiplier(attacking_type, pokemon_types):
-  a = attacking_type
-  p1 = pokemon_types[0]
-  p2 = pokemon_types[1]
-  attacking_type = TYPE_INDEX.get(attacking_type, 1)
-  pokemon_types[0] = TYPE_INDEX.get(pokemon_types[0], 1)
-  pokemon_types[1] = TYPE_INDEX.get(pokemon_types[1], 1)
+def get_multiplier(attacking_type_str, pokemon_types_str_array):
+  if pokemon_types_str_array[1] == '':
+    attacking_type = TYPE_INDEX.get(attacking_type_str, 1)
+    defending_type_1 = TYPE_INDEX.get(pokemon_types_str_array[0], 1)
+    damage_multiplier_1 = TYPE_CHART[attacking_type][defending_type_1]
+    print(f"{attacking_type_str} * {pokemon_types_str_array[0]} = {damage_multiplier_1}")
 
-  damage_multiplier_1 = TYPE_CHART[attacking_type][pokemon_types[0]]
-  damage_multiplier_2 = TYPE_CHART[attacking_type][pokemon_types[1]]
-  print(f"{a} * {p1} = {damage_multiplier_1}")
-  print(f"{a} * {p2} = {damage_multiplier_2}")
-  print(damage_multiplier_2)
+    return damage_multiplier_1
+
+  attacking_type = TYPE_INDEX.get(attacking_type_str, 1)
+  defending_type_1 = TYPE_INDEX.get(pokemon_types_str_array[0], 1)
+  defending_type_2 = TYPE_INDEX.get(pokemon_types_str_array[1], 1)
+  damage_multiplier_1 = TYPE_CHART[attacking_type][defending_type_1]
+  damage_multiplier_2 = TYPE_CHART[attacking_type][defending_type_2]
+  print(f"{attacking_type_str} * {pokemon_types_str_array[0]} = {damage_multiplier_1}")
+  print(f"{attacking_type_str} * {pokemon_types_str_array[1]} = {damage_multiplier_2}")
 
   return damage_multiplier_1 * damage_multiplier_2
 
-attacking_type = 'Psychic'
-pokemon_types = ['Dark', None]
-damage_multiplier = get_multiplier(attacking_type, pokemon_types)
-print(f"Damage Multiplier: {damage_multiplier}")
+# attacking_type = 'Psychic'
+# pokemon_types = ['Dark', None]
+# damage_multiplier = get_multiplier(attacking_type, pokemon_types)
+# print(f"Damage Multiplier: {damage_multiplier}")
